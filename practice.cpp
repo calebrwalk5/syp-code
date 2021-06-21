e319e24374d6605621112147b9ecc10c2c083c49
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-06-21 10:47:01 -0400

    Add files via upload

--- practice.cpp
+++ practice.cpp
@@ -2,24 +2,40 @@
 using namespace std;
 
 /*
- * MTU Summer Youth Program practice C++ program
-*/
+ *  * MTU Summer Youth Program practice C++ program
+ *  */
+
+int fizzbuzz() {
+  for (int i = 1; i < 100; i++) {
+    if((i % 15) == 0) {
+      std::cout << "fizzbuzz\n";
+    } else if ((i % 3) == 0) {
+      std::cout << "buzz\n";
+    } else if ((i % 5) == 0) {
+      std::cout << "fizz\n";
+    } else {
+      std::cout << i << "\n";
+    }
+  }
+}
 
 int main () {
-        fizzbuzz();
+	fizzbuzz;
+	int coolness = 1;
+	bool student = true;
+	bool epic = true;
+
+	if (student == true) {
+		coolness++;
+	}
+	if (epic == true) {
+		for (;;) {
+			coolness++;
+			std::cout << "coolness: " << coolness << "\n";
+		}
+	}
         return 0;
 }
 
-int fizzbuzz() {
-        for (int i = 1; i < 100; i++) {
-                if((i % 15) == 0) {
-                        std::cout << "fizzbuzz\n";
-                } else if ((i % 3) == 0) {
-                        std::cout << "buzz\n";
-                } else if ((i % 5) == 0) {
-                        std::cout << "fizz\n";
-                } else {
-                        std::cout << i << "\n";
-                }
-        }
-}
+
+
