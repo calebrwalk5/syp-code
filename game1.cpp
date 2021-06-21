2b8f88bad917fd42b06e276899b278050dbf0a93
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-06-21 15:02:16 -0400

    Add files via upload

--- game1.cpp
+++ game1.cpp
@@ -16,24 +16,38 @@ void quit() {
 	}
 }
 
+void win() {
+	for (;;) {
+		system("echo You Win!");
+		system("clear");
+		std::cout << "You Win!";
+	}
+}
+
+void level2() {
+	system("clear");
+	cout << "level 2 is easy";
+	win();
+}
+
 void level1() {
 	system("clear");
 	std::cout << "I'm glad you want to continue\n";
 	std::cout << "Well, kid, welcome to the game\n";
 	choice = 0;
-	system ("clear");
+	system("clear");
 	std::cout << "Welcome to level 1\n";
 	std::cout << "Continue?\n" << "1. Yes\n" << "2. No\n";
 	std::cin >> choice;
 	if (choice == 1) {
 		system("clear");
-		std::cout << "Goblins are attacking the city!\n" << "1. Follow me\n" << "2. Make your own path\n";
+		std::cout << "Goblins are attacking the city!\n" << "1. Make your own path\n" << "2. Follow me\n";
 		std::cin >> choice;
-		if (choice == 2) {
+		if (choice == 1) {
 			std::cout << "YOU DIED\n" << "GAME OVER\n";
-		} else if (choice == 1) {
+		} else if (choice == 2) {
 			std::cout << "Good choice, " << name << ". You're still alive for now\n";
-			std::cin >> choice;
+			level2();
 		} else { std::cout << "what?\n"; }
 	} else if (choice == 2) {
 		quit();
