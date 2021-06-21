12c78633db2bb7c1415978b8fc08e56a974f061d
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-06-21 10:34:42 -0400

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -6,5 +6,20 @@ using namespace std;
 */
 
 int main () {
-	return 0;
+        fizzbuzz();
+        return 0;
+}
+
+int fizzbuzz() {
+        for (int i = 1; i < 100; i++) {
+                if((i % 15) == 0) {
+                        std::cout << "fizzbuzz\n";
+                } else if ((i % 3) == 0) {
+                        std::cout << "buzz\n";
+                } else if ((i % 5) == 0) {
+                        std::cout << "fizz\n";
+                } else {
+                        std::cout << i << "\n";
+                }
+        }
 }
