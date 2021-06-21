fa3546d1fd933fe49fd05a56d82a8fba234946f6
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-06-21 14:36:20 -0400

    Add files via upload

---
+++ game1.cpp
@@ -0,0 +1,78 @@
+#include "iostream"
+#include "stdlib.h"
+using namespace std;
+
+/*
+ * Text-based game for SYP
+*/
+
+string name = "";
+bool running = 0;
+int choice = 1;
+
+void quit() {
+	for (;;) {
+		std::cout << "there is no escape\n";
+	}
+}
+
+void level1() {
+	system("clear");
+	std::cout << "I'm glad you want to continue\n";
+	std::cout << "Well, kid, welcome to the game\n";
+	choice = 0;
+	system ("clear");
+	std::cout << "Welcome to level 1\n";
+	std::cout << "Continue?\n" << "1. Yes\n" << "2. No\n";
+	std::cin >> choice;
+	if (choice == 1) {
+		system("clear");
+		std::cout << "Goblins are attacking the city!\n" << "1. Follow me\n" << "2. Make your own path\n";
+		std::cin >> choice;
+		if (choice == 2) {
+			std::cout << "YOU DIED\n" << "GAME OVER\n";
+		} else if (choice == 1) {
+			std::cout << "Good choice, " << name << ". You're still alive for now\n";
+			std::cin >> choice;
+		} else { std::cout << "what?\n"; }
+	} else if (choice == 2) {
+		quit();
+	} else { 
+		std::cout << "error\n";
+	}
+}
+
+void game() {
+	if (running == 1) {
+		system("clear");
+		std::cout << "What is your name? ";
+		std::cin >> name;
+		std::cout << "Hello, " << name << ". Welcome to my personal hell.\n";
+		std::cout << "Press any key and enter to continue\n";
+		string i = "";
+		std::cin >> i;
+		if (i != "") {
+		for (;;) {
+			system("clear");
+			std::cout << "You're talking to a computer right now, and that's pretty sad.\n" << "\n1. Carry on" << "\n2. Give up\n";
+			std::cin >> choice;
+			if (choice == 1) {
+				level1();
+				} else if (choice == 2) {
+					quit();
+				} else {
+					std::cout << "what?\n";
+				break;
+				}
+			}
+		} else {
+			std::cout << "error\n";
+		}
+	}	
+}
+
+int main() {
+	running = 1;
+	std::cout << "game running...\n";
+	game();
+}
